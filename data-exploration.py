9e5e35cfc628d565970f3c4db6e2930a1dd225ba
Kir1m1 <kirimi@paygoenergy.co>
2024-10-29 18:55:12 +0300

    initial commit

---
+++ data-exploration.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import json
+import argparse
+import os
+#Import any other necessary modules
+
+
+def load_data(path):
+    df = pd.read_csv(path,low_memory=False)
+    return df
+
+
+def get_highest_correlated_feature(df):
+    '''Implement your code to return the feature
+    having the highest correlation with the 'target' variable'''
+    
+    return
+
+
+
+def category_with_highest_mean_cat_feature_2(df):
+    '''Implement your code to return the category in 'cat_feature_2'
+    variable which has the highest mean for the 'target' variable'''
+    
+    return
+
+
+
+def abs_std_dev_diff_btwn_groups_cat_feature_1(df):
+    '''Implement your code to return the absolute difference in standar deviation
+    of the 'target' variable between the 2 groups present in 'cat_feature_1' '''
+    
+    return
+
+
+
+def min_feature_8_for_cat_feature_2(df):
+    '''Implement your code to return the minimum value of 'feature_8'
+    for the group Category_C in the variable 'cat_feature_2' '''
+    
+    return
+
+
+
+def get_variance_feature_12_for_group(df):
+    '''Implement your code to return the variance of 'feature_12' for the 
+    group where 'cat_feature_2' is Category_A and 'cat_feature_1' is High'''
+    
+    return
+
+
+
+
+def main():
+    cur_dir = os.getcwd()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_dir',type=str,default=cur_dir)
+    parser.add_argument('--data_dir',type=str,default='')
+    parser.add_argument('--dataset',type=str,default='case_study_data.csv')
+    parser.add_argument('--output_dir',type=str,default='output')
+    parser.add_argument('--output_name',type=str,default="calculated_metrics.json")
+    args = parser.parse_args()
+
+    data_path = os.path.join(cur_dir,args.data_dir,args.dataset)
+    df = load_data(data_path)
+    results = {}
+    results['get_highest_correlated_feature'] = get_highest_correlated_feature(df)
+    results['category_with_highest_mean_cat_feature_2'] = category_with_highest_mean_cat_feature_2(df)
+    results['abs_std_dev_diff_btwn_groups_cat_feature_1'] = round(abs_std_dev_diff_btwn_groups_cat_feature_1(df),2)
+    results['min_feature_8_for_cat_feature_2'] = round(min_feature_8_for_cat_feature_2(df),2)
+    results['get_variance_feature_12_for_group'] = round(get_variance_feature_12_for_group(df),2)
+
+    output_path = os.path.join(args.base_dir, args.output_dir, args.output_name)
+    with open(output_path, "w") as outfile: 
+        json.dump(results, outfile)
+
+
+if __name__ == '__main__':
+    main()
---
+++ inference.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error, root_mean_squared_error
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import svm
+import argparse
+import os
+import pickle
+import json
+
+
+def read_data(path):
+    df = pd.read_csv(path,low_memory=False)
+    return df
+    
+
+def load_model(model_path):
+    model = pickle.load(open(model_path,"rb"))
+    return model
+    
+    
+def preprocessing(df):
+    '''Write your code to preprocess the dataframe to generate your features to be passed to the model and return the preprocessed dataframe'''
+
+    return df_processed
+    
+
+def inference(df_processed, model):
+    '''Write your code to pass the preprocessed dataframe to your model and generate predictions from the model and return the predictions'''
+
+    return preds
+    
+    
+    
+def main():
+    cur_dir = os.getcwd()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_dir',type=str,default=cur_dir)
+    # parser.add_argument('--data_dir',type=str,default='data')
+    # parser.add_argument('--model_dir',type=str,default='models')
+    parser.add_argument('--dataset',type=str,default='case_study_validation_data.csv')
+    parser.add_argument('--model_name',type=str,default='model.pkl')
+    parser.add_argument('--output_dir',type=str,default='output')
+    parser.add_argument('--output_name',type=str,default="inference_results.json")
+    args = parser.parse_args()
+    
+    cur_dir = args.base_dir
+    # data_dir = os.path.join(cur_dir,args.data_dir)
+    # model_dir = os.path.join(cur_dir,args.model_dir)
+    # dataset_path = os.path.join(data_dir,args.dataset_name)
+    # model_path = os.path.join(model_dir,args.model_name)
+    dataset_path = os.path.join(cur_dir,args.dataset)
+    model_path = os.path.join(cur_dir,args.model_name)
+    
+    df = read_data(dataset_path)
+    model = load_model(model_path)
+    
+    df_processed = preprocessing(df)
+    
+    predictions = inference(df_processed[[col for col in df_processed.columns if col != 'target']], model)
+    
+    print("The Root Mean Squared Error for the model is {}".format(root_mean_squared_error(df_processed['target'], predictions)))
+    
+    results = {}
+    results['rmse'] = root_mean_squared_error(df_processed['target'], predictions)
+    output_path = os.path.join(args.base_dir, args.output_dir, args.output_name)
+    with open(output_path, "w") as outfile: 
+        json.dump(results, outfile)
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+
+    
